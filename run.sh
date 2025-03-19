888137ab112c1ad32c29e448e2f66b24e47c0afa
septicalls <vyasamnayak@@gmail.com>
2025-03-20 00:42:47 +0530

    compiled the kernel

--- run.sh
+++ run.sh
@@ -4,5 +4,14 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+# C compiler and flags
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# Building the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
 # Starting QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
